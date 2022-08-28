fccfb967a220ec7a2f94f7b8c328930e5c207894
KMAdilandman <khanhdzno1st01061102@gmail.com>
2022-08-29 00:34:39 +0700

    Test

---
+++ 0nhap.py
@@ -0,0 +1,14 @@
+import math
+def prime(n):
+    if n<2:
+        return False
+    if n<4:
+        return True
+    m=int(math.sqrt(n))+1
+    for i in range(2,m):
+        if n%i==0:
+            return False
+    return True
+for _ in range(int(input())):
+    n=int(input())
+    print("YES" if prime(n) else "NO")
